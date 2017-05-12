c9078cf1df91894fbe151d7318005a930e9ab380
somename <someemail>
2017-05-12 20:58:12 +0200

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -74,7 +74,7 @@ public class PlanetExplorer {
 				trenutnaY=x-1;
 				return "("+trenutnaX+","+trenutnaY+","+facing+")";
 				
-			}else if(trenutnaX==0&&facing=="E"){
+			}else if(trenutnaX==0&&facing=="E"||trenutnaX==0&&facing=="W"){
 				
 				trenutnaX=x-1;
 				return "("+trenutnaX+","+trenutnaY+","+facing+")";
