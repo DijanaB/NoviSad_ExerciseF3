18011a8933f5b0ec9ab47de98fa8c4186a9e7c84
somename <someemail>
2017-05-12 20:26:15 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -8,4 +8,17 @@ public class TestPlanetExplorer {
 	public void test_executeCommand() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void testOnceForward(){
+		
+		PlanetExplorer explorer = new PlanetExplorer(3,3);
+		
+		String ocekivano="0,1,N";
+		assertEquals(ocekivano,explorer.executeCommand("f"));
+		
+		
+		
+		
+	}
 }
